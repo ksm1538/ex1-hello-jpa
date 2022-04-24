b8ca721cc6f749a5b3a501505fbae3ae76b81b19
sm <ksm1538@naver.com>
2022-04-24 19:19:19 +0900

    JPA 임베디드 타입 공부

---
+++ src/main/java/com/sampleJpa/ex2/main/jpaMain_Embedded.java
@@ -0,0 +1,46 @@
+package com.sampleJpa.ex2.main;
+
+import com.sampleJpa.ex2.entity.Address;
+import com.sampleJpa.ex2.entity.Member;
+import com.sampleJpa.ex2.entity.Team;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class jpaMain_Embedded {
+    public static void main(String[] args){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+        try{
+            Address address = new Address("city", "street", "100000");
+            Member mem1 = new Member();
+            mem1.setName("네임");
+            mem1.setAddress(address);
+            em.persist(mem1);
+
+            Member mem2 = new Member();
+            mem2.setName("네임2");
+            mem2.setAddress(address);
+            em.persist(mem2);
+
+            // mem1의 city를 바꾸고싶어함.
+            // 하지만 mem2도 바뀌어짐
+            // 임베디드 타입을 여러 엔티티에서 공유하면 위험.
+            // 이걸 막기위해선 새로운 객체를 생성해서 mem2 에 넣어야함(깊은복사로)
+            mem1.getAddress().setCity("changed City");
+
+            tx.commit();
+        } catch(Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
